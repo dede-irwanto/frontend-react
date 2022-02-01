691dae5211116d76638c015930c9a43de6418529
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-02-01 12:30:11 +0800

    update index.jsx

--- src/pages/posts/Index.jsx
+++ src/pages/posts/Index.jsx
@@ -1,13 +1,64 @@
-import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+// import hook useState dan useEffect from react
+import axios from "axios";
+import { React, useState, useEffect } from "react";
+
+// import component react-bootstrap
+import { Button, Card, Col, Container, Row, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const IndexPost = () => {
+  // define state
+  const [posts, setPosts] = useState([]);
+
+  // useEffect hook
+  useEffect(() => {
+    // panggil method "fecthData"
+    fecthData();
+  }, []);
+
+  // function fectData
+  const fecthData = async () => {
+    // fecthing
+    const response = await axios.get("http://localhost:3001/api/posts");
+    // get response data
+    const data = await response.data.data;
+    // assign response data to state "posts"
+    setPosts(data);
+  };
+
   return (
     <Container className="mt-3">
       <Row>
         <Col md="{12}">
           <Card className="border-0 rounded shadow-sm">
-            <Card.Body>HALAMAN INDEX POST</Card.Body>
+            <Card.Body>
+              <Button
+                as={Link}
+                to="/posts/create"
+                variant="success"
+                className="mb-3"
+              >
+                TAMBAH POST
+              </Button>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>NO.</th>
+                    <th>TITLE</th>
+                    <th>CONTENT</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {posts.map((post, index) => (
+                    <tr key={post.id}>
+                      <td>{index + 1}</td>
+                      <td>{post.title}</td>
+                      <td>{post.content}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Card.Body>
           </Card>
         </Col>
       </Row>
