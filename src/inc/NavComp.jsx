9f9f28c42909663ca75184cb96098bd6442b91f9
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-02-01 11:02:50 +0800

    update navcomp.jsx

--- src/inc/NavComp.jsx
+++ src/inc/NavComp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NavComp = () => {
   return (
@@ -9,8 +10,8 @@ const NavComp = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/">HOME</Nav.Link>
+            <Nav.Link href="/posts">POSTS</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
