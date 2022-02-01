53219a2a9550d4858fca70d47a4bcfecad951242
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-02-01 10:42:01 +0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavComp from "./inc/NavComp";
+import Home from "./pages/Home";
+import CreatePost from "./pages/posts/Create";
+import EditPost from "./pages/posts/Edit";
+import IndexPost from "./pages/posts/Index";
 
 const App = () => {
   return (
     <div>
-      <h1>Belajar React JS</h1>
+      <NavComp />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<IndexPost />} />
+          <Route path="/" element={<CreatePost />} />
+          <Route path="/" element={<EditPost />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
