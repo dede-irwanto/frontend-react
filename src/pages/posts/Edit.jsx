50bf6165e5ff4442a223cef5410f08d383a31256
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-02-02 07:54:07 +0800

    udate edit.jsx

--- src/pages/posts/Edit.jsx
+++ src/pages/posts/Edit.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import axios from "axios";
 import { Card, Col, Container, Row } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
 
 const EditPost = () => {
+  // state
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  // state validation
+  const [validation, setValidation] = useState({});
+
+  // navigate
+  const navigate = useNavigate();
+
+  // get id from paramater
+  const { id } = useParams();
+
+  // hook effect
+  useEffect(() => {
+    // panggil fungsi getPostByID()
+    getPostByID();
+  });
+
+  // functionn get post by id
+  const getPostByID = async () => {
+    // get data from server
+    const response = await axios.get(`http://localhost:3001/api/posts/${id}`);
+    // get response data
+    const data = await response.data.data;
+
+    // assign data to state
+    setTitle(data.title);
+  };
+
   return (
     <Container className="mt-3">
       <Row>
