d49cae55fe2390b13622e1eff5a5dace080ae628
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-02-02 00:09:42 +0800

    update create.jsx

--- src/pages/posts/Create.jsx
+++ src/pages/posts/Create.jsx
@@ -1,13 +1,96 @@
-import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import axios from "axios";
+import { useState } from "react";
+import {
+  Card,
+  Col,
+  Container,
+  Row,
+  Form,
+  Button,
+  Alert,
+} from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
+  // state
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  // state validation
+  const [validation, setValidation] = useState({});
+
+  // show alert
+  const [show, setShow] = useState(true);
+
+  // navigate
+  const navigate = useNavigate();
+
+  // method store POST
+  const storePost = async (e) => {
+    e.preventDefault();
+
+    // send data to server
+    await axios
+      .post("http://localhost:3001/api/posts/store", {
+        title: title,
+        content: content,
+      })
+      .then(() => {
+        // redirect
+        navigate("/posts");
+      })
+      .catch((error) => {
+        // assign validation on state
+        setValidation(error.response.data);
+      });
+  };
+
   return (
     <Container className="mt-3">
       <Row>
         <Col md="{12}">
           <Card className="border-0 rounded shadow-sm">
-            <Card.Body>HALAMAN CREATE POST</Card.Body>
+            <Card.Body>
+              {validation.errors && (
+                <div>
+                  <Alert variant="danger">
+                    <Alert.Heading>Error!</Alert.Heading>
+                    {validation.errors.map((error, index) => (
+                      <p key={index}>
+                        {error.param} : {error.msg}
+                      </p>
+                    ))}
+                  </Alert>
+                </div>
+              )}
+              <Form onSubmit={storePost}>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label>TITLE</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Masukkan Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>CONTENT</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    placeholder="Masukkan Content"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  SIMPAN
+                </Button>
+              </Form>
+            </Card.Body>
           </Card>
         </Col>
       </Row>
