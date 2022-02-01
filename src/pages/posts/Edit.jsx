f58ce35c85f7972ad56595448dea0752b1aac90e
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2022-02-01 10:23:54 +0800

    add Edit.js

---
+++ src/pages/posts/Edit.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+
+const EditPost = () => {
+  return (
+    <Container>
+      <Row>
+        <Col md="{12}">
+          <Card className="border-0 rounded shadow-sm">
+            <Card.Body>HALAMAN EDIT POST</Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default EditPost;
